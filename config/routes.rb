9375a75cfa12e792a8d6c8a891b060623eb8e2cb
Jason Ainsworth <jasoncruze@gmail.com>
2017-10-05 13:18:26 -0700

    commit before attempting to ajax the signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-
-
   root to: "pages#home"
+  # devise_for :users
+  #   devise_scope :user do
+  #     authenticated :user do
+  #       root :to => 'conversations#index'
+  #     end
+  #     unauthenticated :user do
+  #       root :to => 'devise/registrations#new', as: :unauthenticated_root
+  #     end
+  #   end
+
+
 
   resources :conversations, only: [:index, :show] do
     resources :messages
